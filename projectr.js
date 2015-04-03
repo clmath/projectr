f6162ee6800ebdd3986fc9afaa638b835325bea9
Clément Mathieu <clement.mathieu@outlook.com>
2015-04-03 11:24:34 +0200

    change the behavior to act only on one repository

--- projectr.js
+++ projectr.js
@@ -1,10 +1,4 @@
-var q = require("promised-io/promise");
-var chalk = require("chalk");
-var login = require("./lib/login");
-var askRepos = require("./lib/askRepos");
-var release = require("./lib/release");
-var deployDoc = require("./lib/deployDoc");
-
+// Get command-line arguments
 var yargs = require("yargs")
 	.usage("Usage: node projectr.js [--release] [--doc] [--build] [--dry-run] [--dir path] [--skip-clone]")
 	.alias("b", "build")
@@ -14,24 +8,40 @@ var yargs = require("yargs")
 	.alias("r", "release")
 	.describe("r", "Make a new release on GitHub and Npm")
 	.describe("dry-run", "Only perform local changes")
-	.describe("dir", "Specify the directory in which the projects are cloned")
+	.describe("dir", "Specify a tmp directory to work")
 	.default("dir", "tmp")
-	.describe("skip-clone", "If the repository already exist in the directory, don't clone it again")
+//	.describe("skip-clone", "If the repository already exist in the directory, don't clone it again")
 	.help("help")
 	.argv;
 
-var user,
-	repos;
+// Set global configuration variable
+GLOBAL.dryRun = yargs.dryRun;
+//GLOBAL.skipClone = yargs.skipClone;
 
+// Set local configuration variable
 var dir = yargs.dir;
+var user,
+	repoName;
+
+// Load dependencies once the globals have been set
+var q = require("promised-io/promise");
+var chalk = require("chalk");
+var fs = require("fs-extra");
+var askRepo = require("./lib/askRepo");
+var deployDoc = require("./lib/deployDoc");
+var login = require("./lib/login");
+var release = require("./lib/release");
+
+fs.mkdirpSync(dir);
 
 var steps = [
 	function () {
+		// login() will set GLOBAL.username and GLOBAL.password
 		return login();
 	},
 	function () {
-		return askRepos(dir, yargs.skipClone).then(function (ans) {
-			repos = ans.repos;
+		return askRepo().then(function (ans) {
+			repoName = ans.repoName;
 			user = ans.user;
 		});
 	}
@@ -41,69 +51,18 @@ var all = !(yargs.release || yargs.doc || yargs.build);
 
 if (all || yargs.release) {
 	steps.push(function () {
-		return release(dir, user, repos);
+		return release(dir, user, repoName);
 	});
 }
 
 if (all || yargs.doc) {
 	steps.push(function () {
-		return deployDoc(user, dir, repos);
+		return deployDoc(dir, user, repoName);
 	});
 }
+
+//TODO Add a step for the build
 
 q.seq(steps).then(function () {
 	console.log(chalk.green("All done!"));
 });
-
-
-/*
-var askRepos = require("./askRepos");
-var updateMetaFiles = require("./updateMetaFiles");
-var releaseGH = require("./releaseGH");
-var releaseNpm = require("./releaseNpm");
-var deployDoc = require("./deployDoc");
-var rimraf = require("rimraf");
-var q = require("promised-io/promise");
-
-var fs = require("fs");
-var chalk = require("chalk");
-
-var tmpDir = "tmpRelease";
-
-var user;
-var repos;
-
-var skipClone = process.argv[2] === "true";
-
-if (!skipClone) {
-	rimraf.sync(tmpDir);
-	fs.mkdirSync(tmpDir);
-}
-
-var steps = [
-	function () {
-		return login();
-	},
-	function () {
-		return askRepos(tmpDir, skipClone);
-	},
-	function (ans) {
-		repos = ans.repos;
-		user = ans.user;
-		return updateMetaFiles(tmpDir, repos);
-	}
-];
-
-if (null) {
-	steps.push(function () {
-		return releaseGH(user, repos);
-	});
-	steps.push(function () {
-		return releaseNpm(tmpDir, repos);
-	});
-}
-steps.push(function () {
-	deployDoc(tmpDir, repos);
-});
-
-q.seq(steps);*/
